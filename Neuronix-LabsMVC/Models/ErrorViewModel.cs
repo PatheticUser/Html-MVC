f3ab4f972bae190e9ff7c327d07d9c9cfd5ec077
Muhammad Rameez <rameezalipacific@gmail.com>
2025-02-06 19:46:32 +0500

    Failed_Attempt

---
+++ Neuronix-LabsMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Neuronix_LabsMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
