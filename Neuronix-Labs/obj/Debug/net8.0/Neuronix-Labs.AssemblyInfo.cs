2551e76347c424e2d574bd76aea6551ce4af0a71
Muhammad Rameez <rameezalipacific@gmail.com>
2025-02-23 21:07:55 +0500

    Moved "Services"

--- Neuronix-Labs/obj/Debug/net8.0/Neuronix-Labs.AssemblyInfo.cs
+++ Neuronix-Labs/obj/Debug/net8.0/Neuronix-Labs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Neuronix-Labs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea36e65a0f9c676e3141bf6e13942de92ae2d97a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea3d81056ce6f2e6c0ea1b8726d19f26d1782aa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Neuronix-Labs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Neuronix-Labs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
